f27106ef7a779775ef048130a7dac1be3d3950fa
lingd600 <gd@linguangdedeMac.local>
2016-05-22 14:07:46 +0800

    初始化项目

---
+++ GDPlayerExampleTests/GDPlayerExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  GDPlayerExampleTests.m
+//  GDPlayerExampleTests
+//
+//  Created by 林广德 on 16/5/22.
+//  Copyright © 2016年 林广德. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GDPlayerExampleTests : XCTestCase
+
+@end
+
+@implementation GDPlayerExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
